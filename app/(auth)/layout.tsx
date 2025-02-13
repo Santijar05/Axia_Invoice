afae938755a5853d123e8d2cf0c0b0c25d566f06
Santijar <santiago.jimeneza@autonoma.edu.co>
2025-02-13 10:18:36 -0500

    Modificación nombres carpetas y  creación modules

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,11 @@
+export default function AuthLayout({
+    children
+   }: {
+    children: React.ReactNode;
+   }) {
+     return (
+       <main className="h-[100dvh] flex justify-center items-center bg-slate-100">
+         { children }
+       </main>
+     );
+   }
